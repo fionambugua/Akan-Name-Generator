49dd24a1ca94bffba4ad69ba2df9e5ac0bde8d3e
Fiona <fionambuguanjeri@gmail.com>
2026-04-28 10:27:04 +0300

    Add gender selection dropdown and implement form submission handling

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector(#button);
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+let day = parseInt(document.getElementById("#day").value);
+let month = parseInt(document.getElementById("#month").value);
+let year = parseInt(document.getElementById("#year").value);
+let gender = parseInt(document.getElementById("#gender").value);
+
+let result = document.getElementById("#result");
+
+
+
+});
